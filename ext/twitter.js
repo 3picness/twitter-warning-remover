ba264691d42cac78c9fc8d65639f83913250bd2a
Epicness <emailpub.epicness@gmail.com>
2020-11-07 12:40:58 -0600

    Version 1.0.2

--- ext/twitter.js
+++ ext/twitter.js
@@ -1,15 +1,26 @@
-code = `window.oldJsonParse = window.JSON.parse;
+code = `
+window.oldJsonParse2 = window.JSON.parse;
+window.oldJsonParse = window.oldJsonParse2;
 window.JSON.parse = function(obj) {
-    var y = window.oldJsonParse(obj);
-    if(y.timeline) {
-        if(y.timeline.id.startsWith("Conversation-") && y.timeline.instructions && y.timeline.instructions[0] &&
-           y.timeline.instructions[0].addEntries) {
-               y.timeline.instructions[0].addEntries.entries[0].content.item.content.tweet.innerTombstoneInfo = null;
-               y.timeline.instructions[0].addEntries.entries[0].content.item.content.tweet.innerForwardPivot = null;
-	           y.timeline.instructions[0].addEntries.entries[0].content.item.content.tweet.forwardPivot = null;
-		}
-    }
-    return y;
+	var y = window.oldJsonParse2(obj);
+	if(y.timeline) {
+		  if((y.timeline.id.startsWith("Conversation-") || y.timeline.id.startsWith("Home-")) && y.timeline.instructions && y.timeline.instructions[0] &&
+			 y.timeline.instructions) {
+				for(var i = 0; i < y.timeline.instructions.length; i++) {
+					console.log("asdf");
+					if(y.timeline.instructions[i].addEntries) {
+						for(var j = 0; j < y.timeline.instructions[i].addEntries.entries.length; j++) {
+							if(y.timeline.instructions[i].addEntries.entries[j].content.item) {
+								y.timeline.instructions[i].addEntries.entries[j].content.item.content.tweet.innerTombstoneInfo = null;
+								y.timeline.instructions[i].addEntries.entries[j].content.item.content.tweet.innerForwardPivot = null;
+								y.timeline.instructions[i].addEntries.entries[j].content.item.content.tweet.forwardPivot = null;
+							}
+						}
+					}
+				}
+			}
+	}
+     return y;
 }`;
 	var script = document.createElement("script");
 	script.setAttribute("nonce", "");
