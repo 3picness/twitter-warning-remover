206f19a8c4c1b95e7548ba025abfa2d48442a844
Epicness <emailpub.epicness@gmail.com>
2020-11-07 13:04:12 -0600

    Version 1.0.3 - Bugfix

--- ext/twitter.js
+++ ext/twitter.js
@@ -3,24 +3,27 @@ window.oldJsonParse2 = window.JSON.parse;
 window.oldJsonParse = window.oldJsonParse2;
 window.JSON.parse = function(obj) {
 	var y = window.oldJsonParse2(obj);
+	console.log(y);
 	if(y.timeline) {
-		  if((y.timeline.id.startsWith("Conversation-") || y.timeline.id.startsWith("Home-")) && y.timeline.instructions && y.timeline.instructions[0] &&
-			 y.timeline.instructions) {
-				for(var i = 0; i < y.timeline.instructions.length; i++) {
-					console.log("asdf");
-					if(y.timeline.instructions[i].addEntries) {
-						for(var j = 0; j < y.timeline.instructions[i].addEntries.entries.length; j++) {
-							if(y.timeline.instructions[i].addEntries.entries[j].content.item) {
-								y.timeline.instructions[i].addEntries.entries[j].content.item.content.tweet.innerTombstoneInfo = null;
-								y.timeline.instructions[i].addEntries.entries[j].content.item.content.tweet.innerForwardPivot = null;
-								y.timeline.instructions[i].addEntries.entries[j].content.item.content.tweet.forwardPivot = null;
-							}
+		if((y.timeline.id.startsWith("Conversation-") || y.timeline.id.startsWith("Home-")) && y.timeline.instructions && y.timeline.instructions[0] &&
+		    y.timeline.instructions) {
+			for(var i = 0; i < y.timeline.instructions.length; i++) {
+				console.log("asdf");
+				if(y.timeline.instructions[i].addEntries) {
+					for(var j = 0; j < y.timeline.instructions[i].addEntries.entries.length; j++) {
+						if(y.timeline.instructions[i].addEntries.entries[j].content.item && y.timeline.instructions[i].addEntries.entries[j].entryId.startsWith("tweet-")) {
+							console.log("Filtering...");
+							y.timeline.instructions[i].addEntries.entries[j].content.item.content.tweet.innerTombstoneInfo = null;
+							y.timeline.instructions[i].addEntries.entries[j].content.item.content.tweet.innerForwardPivot = null;
+							y.timeline.instructions[i].addEntries.entries[j].content.item.content.tweet.forwardPivot = null;
 						}
 					}
 				}
 			}
+		}
 	}
-     return y;
+	console.log(y);
+    return y;
 }`;
 	var script = document.createElement("script");
 	script.setAttribute("nonce", "");
