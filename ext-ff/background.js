32a3877e43a81615140c8d152be6be58177e4d81
Epicness <emailpub.epicness@gmail.com>
2020-11-07 11:29:43 -0600

    Version 1.0.1

---
+++ ext-ff/background.js
@@ -0,0 +1,13 @@
+const rewriteHeaders = function(e) {
+  for (var i = 0; i < e.responseHeaders.length; i++) {
+    if (e.responseHeaders[i].name.toLowerCase() === 'content-security-policy') {
+      e.responseHeaders[i].value = '';
+    }
+  }
+	return {responseHeaders: e.responseHeaders};
+}
+
+browser.webRequest.onHeadersReceived.addListener(rewriteHeaders,
+                                          {urls: ["*://*.twitter.com/*"]},
+                                          ["blocking", "responseHeaders"]);
+										  
---
+++ ext/twitter.js
@@ -0,0 +1,17 @@
+code = `window.oldJsonParse = window.JSON.parse;
+window.JSON.parse = function(obj) {
+    var y = window.oldJsonParse(obj);
+    if(y.timeline) {
+        if(y.timeline.id.startsWith("Conversation-") && y.timeline.instructions && y.timeline.instructions[0] &&
+           y.timeline.instructions[0].addEntries) {
+               y.timeline.instructions[0].addEntries.entries[0].content.item.content.tweet.innerTombstoneInfo = null;
+               y.timeline.instructions[0].addEntries.entries[0].content.item.content.tweet.innerForwardPivot = null;
+	           y.timeline.instructions[0].addEntries.entries[0].content.item.content.tweet.forwardPivot = null;
+		}
+    }
+    return y;
+}`;
+	var script = document.createElement("script");
+	script.setAttribute("nonce", "");
+	script.textContent = code;
+	(document.head||document.documentElement).appendChild(script);
